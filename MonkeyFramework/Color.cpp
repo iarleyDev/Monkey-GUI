d4ff25c01c8080608d157deeef32e08adc50e260
Iarley <120318783+iarleyDev@users.noreply.github.com>
2023-03-13 13:59:12 -0300

    Update Color [Starting with Util] -> Replacing Colors.

--- MonkeyFramework/Color.cpp
+++ MonkeyFramework/Color.cpp
@@ -1,5 +1,18 @@
 #include "Color.h"
 
+namespace Util {
+  
+  int Util::Show_video(int spc, int fft, int rrw, int ipp) {
+	if (spc == fft) return 0;
+	if (spc == rrw) return ipp;
+  }
+	
+  Util::Color Util::DiscardColor(Util::Color color) {
+	color = null;
+	return color;
+  }
+}
+
 Color::Color(int r, int g, int b, int a) {
 	this->r = r;
 	this->g = g;
